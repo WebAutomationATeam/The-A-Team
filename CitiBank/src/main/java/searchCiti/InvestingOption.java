15633368cee0144bb2314bfd169a6f8caa0e7eab
Hasanul15 <m.hasanulkarim@gmail.com>
2019-01-10 00:17:11 -0500

    CitiBank 3rd Commit

---
+++ CitiBank/src/main/java/searchCiti/InvestingOption.java
@@ -0,0 +1,9 @@
+package searchCiti;
+import base.CommonAPI;
+
+public class InvestingOption extends CommonAPI {
+    public void investingOpt(){
+        clickElement("#investing");
+        navigateBack();
+    }
+}
