488c25a252c2b3fbe09ec5a3c9d8f3344aca5bce
animeshpartha <partha_uk@hotmail.com>
2019-01-10 00:26:28 -0500

    Costco Forth Commit

---
+++ Costco/src/test/java/TestCustomerService.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+import search.SearchCustomerService;
+
+public class TestCustomerService extends SearchCustomerService {
+
+    @Test
+    public void test3(){
+        searchCustomerService();
+    }
+}
