c78ae8ef0a1325cbc2b585af335c8b799c820019
Hasanul15 <m.hasanulkarim@gmail.com>
2019-01-10 00:48:12 -0500

    CitiBank 8th commit

---
+++ CitiBank/src/test/java/TestLendingOpt.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+import searchCiti.LendingOption;
+
+public class TestLendingOpt extends LendingOption {
+    @Test
+        public void test() {
+        lendingOpt();
+    }
+}
+
