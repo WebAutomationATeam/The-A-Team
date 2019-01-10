199ac0240b9ac7993c139b3f2751a645966db87d
Hasanul15 <m.hasanulkarim@gmail.com>
2019-01-09 19:36:55 -0500

    push to Hasanul

---
+++ CitiBank/src/test/java/TestBankingOpt.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+import searchCiti.BankingOption;
+
+public class TestBankingOpt extends BankingOption {
+    @Test
+    public void test(){
+        bankingOpt();
+    }
+}
