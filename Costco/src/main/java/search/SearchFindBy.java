8bbf06e5dd47d241f4734067c300e32831dbd9ff
animeshpartha <partha_uk@hotmail.com>
2019-01-11 16:12:02 -0500

    Fourteen Commit

--- Costco/src/main/java/search/SearchFindBy.java
+++ Costco/src/main/java/search/SearchFindBy.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 public class SearchFindBy extends CommonAPI {
 
+    //SearchButton
     @FindBy(how = How.XPATH, using = "/html[1]/body[1]/header[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[2]/div[1]/form[1]/div[2]/span[1]/input[2]")
     public WebElement searchButton;
     public WebElement getSearchButton() {
@@ -23,17 +24,19 @@ public class SearchFindBy extends CommonAPI {
         getSearchButton().click();
     }
 
-    @FindBy(how = How.XPATH, using = "/html[1]/body[1]/header[1]/div[1]/div[1]/div[1]/div[1]/a[1]/img[1]")
-    public WebElement getCostco;
-    public WebElement getGetCostco() {
-        return getCostco;
+    //Costco All Button
+    @FindBy(how = How.XPATH, using = "/html[1]/body[1]/header[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[2]/div[1]/form[1]/div[1]/select[1]")
+    public WebElement getCostcoAll;
+    public WebElement getGetCostcoAll() {
+        return getCostcoAll;
     }
-    public void clickonGetCostco() {
+    public void clickonGetCostcoAll() {
         TestLogger.log(getClass().getSimpleName() + ":" + convertToString(new Object() {
         }.getClass().getEnclosingMethod().getName()));
-        getGetCostco().click();
+        getGetCostcoAll().click();
     }
 
+    //Signin
     @FindBy(how = How.XPATH, using = "/html[1]/body[1]/header[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[3]/ul[1]/li[2]/a[1]")
     public WebElement signIn;
     public WebElement getSignIn() {
@@ -45,7 +48,7 @@ public class SearchFindBy extends CommonAPI {
         getSignIn();
     }
 
-    // Costco Logo
+    //Costco Logo
     @FindBy (css="body.en div.container-fluid.fixed-container div.table-container.parent div.table-row div." +
             "table-cell.logo.hidden-xs.hidden-sm.hidden-md.hidden-lg:nth-child(1) a:nth-child(1) > img.bc-logo.logo-us")
     public WebElement CostcoLogo;
@@ -71,32 +74,41 @@ public class SearchFindBy extends CommonAPI {
     public WebElement emailoffer;
     public void clickEmailOffer () { emailoffer.sendKeys("Game of Thrones", Keys.ENTER); }
 
-    //click grocery
+    //Grocery
     @FindBy(css = "#Home_Ancillary_0")
     public static WebElement grocery;
     public void clickGrocery(){grocery.click();}
 
-    //click business delivery
+    //Business delivery
     @FindBy(css = "#Home_Ancillary_1")
     public static WebElement businessdelivery;
     public void clickBusinessDelivdery(){businessdelivery.click();}
 
-    //optical
+    //Optical
     @FindBy(css = "#Home_Ancillary_2")
     public static WebElement searchoptical;
     public void clickOptical(){searchoptical.click();}
 
-    //pharmacy
+    //Pharmacy
     @FindBy(css = "#Home_Ancillary_3")
     public static WebElement pharmacy;
-    public void getPharmacy(){pharmacy.click();}
+    public void clickPharmacy(){pharmacy.click();}
 
+    //Photo
+    @FindBy(css = "#Home_Ancillary_5")
+    public static WebElement photo;
+    public void clickPhoto(){photo.click();}
 
+    //Travel
+    @FindBy(css = "#Home_Ancillary_6")
+    public static WebElement travel;
+    public void clickTravel(){travel.click();}
 
-
-
-
-
+    //slide
+    @FindBy(css = "body.en div.genericEspot.container-fluid.fixed-container.WC_ContentAreaESpot_div_1_Home_Hero_Carousel" +
+            ":nth-child(91) div.slick-initialized.slick-slider.slick-dotted > button.slick-prev.slick-arrow")
+    public static WebElement slide;
+    public void clickSlide(){slide.click();}
 
 
 
