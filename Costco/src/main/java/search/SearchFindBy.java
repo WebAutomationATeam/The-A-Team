f42ccc37e78bd83c7ed6f28e6aefd8a3b59db7cf
animeshpartha <partha_uk@hotmail.com>
2019-01-11 16:18:51 -0500

    Sixteen Commit

--- Costco/src/main/java/search/SearchFindBy.java
+++ Costco/src/main/java/search/SearchFindBy.java
@@ -115,6 +115,11 @@ public class SearchFindBy extends CommonAPI {
             ":nth-child(91) div.slick-initialized.slick-slider.slick-dotted > button.slick-next.slick-arrow")
     public static WebElement slide1;
 
+    //Cart
+    @FindBy(xpath = "/html[1]/body[1]/header[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[3]/ul[1]/li[3]/a[1]")
+    public static WebElement cart;
+    public void clickCart(){cart.click();}
+
 }
 
 
