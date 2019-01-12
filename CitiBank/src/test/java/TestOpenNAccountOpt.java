8cc577a0f95daa81c19e57362900de8915f67855
Hasanul15 <m.hasanulkarim@gmail.com>
2019-01-11 19:07:41 -0500

    two test case created

---
+++ CitiBank/src/test/java/TestOpenNAccountOpt.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+import topHeader.OpenNAccountOption;
+
+public class TestOpenNAccountOpt extends OpenNAccountOption {
+
+    @Test
+    public void test() {
+        openNAccountOpt();
+    }
+}
