a3b112f05207f54a798e8c2b0838b0af0143495a
animeshpartha <partha_uk@hotmail.com>
2019-01-11 19:06:56 -0500

    Nineteen Commit

--- Costco/src/test/java/testCostcoSearching/TestDB.java
+++ Costco/src/test/java/testCostcoSearching/TestDB.java
@@ -5,21 +5,22 @@ import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import searchDB.DBOperation;
 
 import java.util.List;
 
 public class TestDB extends CommonAPI {
 
-    DatabaseOperation databaseOperation;
+    DBOperation dbOperation;
     @BeforeMethod
     public void setUP(){
-        databaseOperation = PageFactory.initElements(driver, DatabaseOperation.class);
+        dbOperation = PageFactory.initElements(driver, DBOperation.class);
 
     }
     @Test
     public void webElementName() throws Exception {
-        List actual = databaseOperation.getUserDatafromDB();
-        List expected = databaseOperation.getHeadersValue1();
+        List actual = dbOperation.getUserDatafromDB();
+        List expected = dbOperation.getHeadersValue1();
         Assert.assertEquals(actual,expected);
 
     }
